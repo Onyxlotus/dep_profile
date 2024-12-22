b1a8dd2c52436ef15217d726e83c6ff12011e154
Onyxlotus <Onyxlotus@yandex.ru>
2024-12-22 23:22:16 +0300

    update App

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         <Navbar />
 
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/dep_profile" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/project/:id" element={<Project />} />
           <Route path="/contacts" element={<Contacts />} />
